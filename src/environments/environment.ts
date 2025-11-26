79cce6f0485df677914f212e9380efcf7c997b20
giovanasilverio <giovanasilveriopereiratp@gmail.com>
2025-11-26 11:25:55 -0300

    feat: integrate AngularFire for Firebase authentication

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAgznjtrgXJG5bTDSA-Mop0XGbjCMC91IQ",
+    authDomain: "searchmovies-3c2fb.firebaseapp.com",
+    projectId: "searchmovies-3c2fb",
+    storageBucket: "searchmovies-3c2fb.firebasestorage.app",
+    messagingSenderId: "17889520696",
+    appId: "1:17889520696:web:e10249a0c568fd646b4acd",
+    measurementId: "G-PZCMSQ2V0Z"
+  }
+};
